03e26b288c7609634dcecadffac36e43fefca9bf
A.E. Szalo <a.e.szalo@gmail.com>
2016-08-08 18:02:24 +0200

    quick::Vtk::SmartVolumeMapper example added

--- src/Vtk/Mapper/AbstractMapper3D/AbstractVolumeMapper/VolumeMapper/quickVtkSmartVolumeMapper.cpp
+++ src/Vtk/Mapper/AbstractMapper3D/AbstractVolumeMapper/VolumeMapper/quickVtkSmartVolumeMapper.cpp
@@ -7,6 +7,27 @@ namespace quick {
         Qml::Register::Class<SmartVolumeMapper> SmartVolumeMapper::Register;
 
         SmartVolumeMapper::SmartVolumeMapper() : VolumeMapper(vtkSmartPointer<vtkSmartVolumeMapper>::New()) {
+            this->m_vtkObject = vtkSmartVolumeMapper::SafeDownCast(this->Algorithm::getVtkObject());
+        }
+
+        auto SmartVolumeMapper::setFinalColorWindow(float finalColorWindow) -> void {
+            this->m_vtkObject->SetFinalColorWindow(finalColorWindow);
+            emit this->finalColorWindowChanged();
+            this->update();
+        }
+
+        auto SmartVolumeMapper::getFinalColorWindow() -> float {
+            return this->m_vtkObject->GetFinalColorWindow();
+        }
+
+        auto SmartVolumeMapper::setFinalColorLevel(float finalColorLevel) -> void {
+            this->m_vtkObject->SetFinalColorLevel(finalColorLevel);
+            emit this->finalColorLevelChanged();
+            this->update();
+        }
+
+        auto SmartVolumeMapper::getFinalColorLevel() -> float {
+            return this->m_vtkObject->GetFinalColorLevel();
         }
     }
 }
--- src/Vtk/Mapper/AbstractMapper3D/AbstractVolumeMapper/VolumeMapper/quickVtkSmartVolumeMapper.hpp
+++ src/Vtk/Mapper/AbstractMapper3D/AbstractVolumeMapper/VolumeMapper/quickVtkSmartVolumeMapper.hpp
@@ -10,10 +10,20 @@ namespace quick {
 
         class SmartVolumeMapper : public VolumeMapper {
             Q_OBJECT
+            Q_PROPERTY(float finalColorWindow READ getFinalColorWindow WRITE setFinalColorWindow NOTIFY finalColorWindowChanged);
+            Q_PROPERTY(float finalColorLevel READ getFinalColorLevel WRITE setFinalColorLevel NOTIFY finalColorLevelChanged);
         private:
             static Qml::Register::Class<SmartVolumeMapper> Register;
+            vtkSmartPointer<vtkSmartVolumeMapper> m_vtkObject;
         public:
             SmartVolumeMapper();
+            auto setFinalColorWindow(float) -> void;
+            auto getFinalColorWindow() -> float;
+            auto setFinalColorLevel(float) -> void;
+            auto getFinalColorLevel() -> float;
+        signals:
+            void finalColorWindowChanged();
+            void finalColorLevelChanged();
         };
     }
 }

c8e7767f395c4a7083adc8893bebc2cf1fef4068
A.E. Szalo <a.e.szalo@gmail.com>
2016-08-09 16:36:44 +0200

    quick::Vtk::HedgeHog added

---
+++ src/Vtk/Algorithm/PolyDataAlgorithm/quickVtkHedgeHog.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "quickVtkPolyDataAlgorithm.hpp"
+
+#include <vtkHedgeHog.h>
+
+namespace quick {
+
+    namespace Vtk {
+
+        class HedgeHog : public PolyDataAlgorithm {
+            Q_OBJECT
+        public:
+            enum VectorMode {
+                UseVector = VTK_USE_VECTOR,
+                UseNormal = VTK_USE_NORMAL
+            };
+            Q_ENUM(VectorMode);
+            Q_PROPERTY(float scaleFactor READ getScaleFactor WRITE setScaleFactor NOTIFY scaleFactorChanged);
+            Q_PROPERTY(VectorMode vectorMode READ getVectorMode WRITE setVectorMode NOTIFY vectorModeChanged);
+        private:
+            static Qml::Register::Class<HedgeHog> Register;
+            vtkSmartPointer<vtkHedgeHog> m_vtkObject;
+        public:
+            HedgeHog();
+            auto setScaleFactor(float) -> void;
+            auto getScaleFactor() -> float;
+            auto setVectorMode(VectorMode) -> void;
+            auto getVectorMode() -> VectorMode;
+        signals:
+            void scaleFactorChanged();
+            void vectorModeChanged();
+        };
+    }
+}

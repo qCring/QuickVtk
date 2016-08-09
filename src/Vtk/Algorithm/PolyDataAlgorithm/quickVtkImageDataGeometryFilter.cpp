38aa28d11098840382d83799e48229ab27109dc4
A.E. Szalo <a.e.szalo@gmail.com>
2016-08-09 16:43:55 +0200

    quick::Vtk::ImageDataGeometryFilter added

---
+++ src/Vtk/Algorithm/PolyDataAlgorithm/quickVtkImageDataGeometryFilter.cpp
@@ -0,0 +1,13 @@
+#include "quickVtkImageDataGeometryFilter.hpp"
+
+namespace quick {
+
+    namespace Vtk {
+
+        Qml::Register::Class<ImageDataGeometryFilter> ImageDataGeometryFilter::Register;
+
+        ImageDataGeometryFilter::ImageDataGeometryFilter() : PolyDataAlgorithm(vtkSmartPointer<vtkImageDataGeometryFilter>::New()) {
+            this->m_vtkObject = vtkImageDataGeometryFilter::SafeDownCast(Algorithm::getVtkObject());
+        }
+    }
+}

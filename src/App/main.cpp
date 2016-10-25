45eaccd241d12740bf73e5c1e3d6244b2bf19eaf
A.E. Szalo <a.e.szalo@gmail.com>
2016-10-25 23:39:11 +0200

    fix: environment variables are now properly set

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -4,15 +4,13 @@
 
 auto main(int argc, char *argv[]) -> int {
 
-#ifdef  __APPLE__
-    setenv("QML_BAD_GUI_RENDER_LOOP", "1", 1);
-    setenv("QML_USE_GLYPHCACHE_WORKAROUND", "1", 1);
-#endif
-
 #ifdef _MSC_VER
     _putenv("QML_BAD_GUI_RENDER_LOOP=1");
     _putenv("QT_AUTO_SCREEN_SCALE_FACTOR=1");
     _putenv("QML_USE_GLYPHCACHE_WORKAROUND=1");
+#else
+    setenv("QML_BAD_GUI_RENDER_LOOP", "1", 1);
+    setenv("QML_USE_GLYPHCACHE_WORKAROUND", "1", 1);
 #endif
 
     return quick::App::Instance::Execute(argc, argv);

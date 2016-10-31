7cf251ca8fc1044b8068d95f02189b401cb34dfe
A.E. Szalo <a.e.szalo@gmail.com>
2016-10-31 17:45:48 +0100

    quick::Code::Formatter added

---
+++ src/Code/quickCodeFormatter.cpp
@@ -0,0 +1,184 @@
+#include "quickCodeFormatter.hpp"
+
+#include "quickCodeEditor.hpp"
+
+#include <QApplication>
+#include <QTextCursor>
+#include <QClipboard>
+#include <QTextBlock>
+
+#include <iostream>
+
+namespace quick {
+
+    namespace Code {
+
+        auto Formatter::setTextDocument(QTextDocument* document) -> void {
+            this->m_document = document;
+        }
+
+        auto Formatter::invalidateText() -> void {
+            Editor::instance->setModified(true);
+        }
+
+        auto Formatter::handleRedo() -> bool {
+            this->m_document->redo();
+            this->invalidateText();
+            return true;
+        }
+
+        auto Formatter::handleUndo() -> bool {
+            this->m_document->undo();
+            this->invalidateText();
+            return true;
+        }
+
+        auto Formatter::handleShowSearch() -> bool {
+            Editor::instance->showSearch();
+            return true;
+        }
+
+        auto Formatter::handlePaste() -> bool {
+            auto text = QApplication::clipboard()->text();
+
+            if (!text.isEmpty())
+            {
+                auto cursor = Editor::instance->getCurrentCursor();
+                cursor.beginEditBlock();
+                cursor.insertText(text);
+                cursor.endEditBlock();
+
+                this->invalidateText();
+            }
+            return true;
+        }
+
+        auto Formatter::handleEnter() -> bool {
+            auto cursor = Editor::instance->getCurrentCursor();
+            cursor.beginEditBlock();
+
+            auto block = cursor.block();
+            auto line = block.text();
+            auto left = line.left(cursor.positionInBlock());
+            auto right = line.right(line.length() - cursor.positionInBlock());
+
+            auto currentLevel = 0;
+            auto newLevel = 0;
+            auto open = left.contains("{");
+            auto close = left.contains("}");
+
+            if (open || !close) {
+
+                if (block.blockNumber() > 0) {
+                    auto prevLevel = block.previous().userState();
+                    prevLevel += prevLevel % 2;
+                    currentLevel = prevLevel;
+                }
+
+                currentLevel += open;
+                newLevel = currentLevel + open;
+
+                cursor.block().setUserState(currentLevel);
+                cursor.insertBlock();
+
+                cursor.block().setUserState(newLevel);
+                auto blockNumber = block.blockNumber();
+
+                auto lines = Editor::instance->getLines();
+                std::cout << "lines: " << lines.count() << std::endl;
+                std::cout << "block: " << blockNumber << std::endl;
+
+                if (blockNumber >= lines.count()) {
+                    lines.append(newLevel);
+                } else {
+                    lines.insert(blockNumber, newLevel);
+                }
+
+                for (auto i = 0; i < newLevel / 2; i++) {
+                    cursor.insertText("\t");
+                }
+
+                Editor::instance->setLines(lines);
+            }
+
+            else {
+                cursor.insertBlock();
+                auto level = block.userState();
+
+                for (auto i = 0; i < level / 2; i++) {
+                    cursor.insertText("\t");
+                }
+            }
+
+            cursor.endEditBlock();
+            this->invalidateText();
+                
+            return true;
+        }
+
+        auto Formatter::handleCloseBraces() -> bool {
+            auto cursor = Editor::instance->getCurrentCursor();
+            auto text = cursor.block().text();
+            auto block = cursor.block();
+            auto line = block.text();
+            auto left = line.left(cursor.positionInBlock());
+            auto right = line.right(line.length() - cursor.positionInBlock());
+
+            auto level = 0;
+
+            if (QRegularExpression("\\w+").match(left).hasMatch()) {
+                cursor.insertText("}");
+                return true;
+            } else {
+                level = std::max(block.userState() - 2, 0);
+                block.setUserState(level);
+            }
+
+            cursor.select(QTextCursor::LineUnderCursor);
+
+            for (auto i = 0; i < level/2; ++i) {
+                cursor.insertText("\t");
+            }
+
+            cursor.insertText("}" + right);
+
+            this->invalidateText();
+
+            return true;
+        }
+
+        auto Formatter::onKeyPressed(int key, int modifiers, const QString& string) -> bool {
+            if (modifiers & Qt::ControlModifier && modifiers & Qt::ShiftModifier && key == Qt::Key_Z) {
+                return handleRedo();
+            }
+
+            if (modifiers == Qt::ControlModifier) {
+                if (key == Qt::Key_F) {
+                    return handleShowSearch();
+                }
+
+                if (key == Qt::Key_Z) {
+                    return handleUndo();
+                }
+
+                if (key == Qt::Key_V) {
+                    return handlePaste();
+                }
+            }
+
+            if (key == Qt::Key_Return || key == Qt::Key_Enter) {
+                return handleEnter();
+            }
+
+            if (string == "}") {
+                return handleCloseBraces();
+            }
+
+            if (QRegularExpression("(\\w+)|(\\W+)").match(string).hasMatch()) {
+                this->invalidateText();
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Code/quickCodeFormatter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QTextDocument>
+
+namespace quick {
+
+    namespace Code {
+
+        class Formatter {
+        private:
+            QTextDocument* m_document = nullptr;
+        private:
+            auto invalidateText() -> void;
+            auto handleUndo() -> bool;
+            auto handleRedo() -> bool;
+            auto handlePaste() -> bool;
+            auto handleEnter() -> bool;
+            auto handleShowSearch() -> bool;
+            auto handleCloseBraces() -> bool;
+        public:
+            auto setTextDocument(QTextDocument*) -> void;
+            auto onKeyPressed(int, int, const QString&) -> bool;
+        };
+    }
+}

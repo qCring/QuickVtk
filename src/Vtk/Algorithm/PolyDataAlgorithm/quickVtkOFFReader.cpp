9df8863769c7b54cb0e5801116419d6c2031055d
A.E. Szalo <a.e.szalo@gmail.com>
2016-11-09 15:34:05 +0100

    quick::Vtk::OFFReader added

---
+++ src/Vtk/Algorithm/PolyDataAlgorithm/quickVtkOFFReader.cpp
@@ -0,0 +1,31 @@
+#include "quickVtkOFFReader.hpp"
+
+#include "quickIO.hpp"
+
+namespace quick {
+
+    namespace Vtk {
+
+        Qml::Register::Symbol::Class<OFFReader> OFFReader::Register;
+
+        OFFReader::OFFReader() : quick::Vtk::PolyDataAlgorithm(vtkSmartPointer<vtkOFFReader>::New()) {
+            this->m_vtkObject = vtkOFFReader::SafeDownCast(getVtkObject());
+        }
+
+        auto OFFReader::setFileName(const QString& fileName) -> void {
+            this->m_fileName = fileName;
+
+            emit this->fileNameChanged();
+
+            if (IO::FileExists(fileName)) {
+                this->m_vtkObject->SetFileName(fileName.toStdString().c_str());
+                this->m_vtkObject->Update();
+                this->update();
+            }
+        }
+
+        auto OFFReader::getFileName() -> QString {
+            return this->m_fileName;
+        }
+    }
+}
---
+++ src/Vtk/Algorithm/PolyDataAlgorithm/quickVtkOFFReader.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "quickVtkPolyDataAlgorithm.hpp"
+
+#include <vtkOFFReader.h>
+
+namespace quick {
+
+    namespace Vtk {
+
+        class OFFReader : public PolyDataAlgorithm {
+            Q_OBJECT
+            Q_PROPERTY(QString fileName READ getFileName WRITE setFileName NOTIFY fileNameChanged);
+        private:
+            static Qml::Register::Symbol::Class<OFFReader> Register;
+            vtkSmartPointer<vtkOFFReader> m_vtkObject;
+            QString m_fileName;
+        public:
+            OFFReader();
+            auto setFileName(const QString&) -> void;
+            auto getFileName() -> QString;
+        signals:
+            void fileNameChanged();
+        };
+    }
+}

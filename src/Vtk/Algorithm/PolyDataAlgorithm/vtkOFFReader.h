afc6eeca97c1744d93a2b2e1e1466c6750141bcd
A.E. Szalo <a.e.szalo@gmail.com>
2016-11-16 10:58:09 +0100

    VTK git tag updated to cf5820e (Nov 16 2016)

--- src/Vtk/Algorithm/PolyDataAlgorithm/vtkOFFReader.h
+++ src/Vtk/Algorithm/PolyDataAlgorithm/vtkOFFReader.h
@@ -6,14 +6,14 @@ class vtkOFFReader : public vtkPolyDataAlgorithm {
 private:
     char *FileName;
 public:
-    static vtkOFFReader *New();
+    static vtkOFFReader* New();
     vtkTypeMacro(vtkOFFReader,vtkPolyDataAlgorithm);
     vtkSetStringMacro(FileName);
     vtkGetStringMacro(FileName);
 protected:
     vtkOFFReader();
-    int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *) override;
-    int RequestData_VtkStyle(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
-    int RequestData_QuickStyle(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
+    int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
+    int RequestData_VtkStyle(vtkInformation*, vtkInformationVector**, vtkInformationVector*);
+    int RequestData_QuickStyle(vtkInformation*, vtkInformationVector**, vtkInformationVector*);
     ~vtkOFFReader();
 };

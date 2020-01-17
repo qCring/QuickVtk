f05b3ca18ac1b53bf08a1d247c10cc7ec2b5c67e
A.E. Szalo <qCring@gmail.com>
2020-01-17 17:47:15 +0100

    Core: Vtk::DensifyPolyData added

---
+++ src/QuickVtk-Core/Vtk/Wrapper/quickVtkDensifyPolyData.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "quickVtkPolyDataAlgorithm.hpp"
+
+#include <vtkDensifyPolyData.h>
+
+namespace quick {
+    namespace Vtk {
+
+        class DensifyPolyData : public PolyDataAlgorithm {
+            Q_OBJECT
+            Q_PROPERTY(int numberOfSubdivisions READ getNumberOfSubdivisions WRITE setNumberOfSubdivisions NOTIFY numberOfSubdivisionsChanged);
+        private:
+            static Qml::Register::Class<DensifyPolyData> Register;
+            vtkSmartPointer<vtkDensifyPolyData> m_vtkObject = nullptr;
+        public:
+            DensifyPolyData();
+            auto setNumberOfSubdivisions(int) -> void;
+            auto getNumberOfSubdivisions() -> int;
+        signals:
+            void numberOfSubdivisionsChanged();
+        };
+    }
+}

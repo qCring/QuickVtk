1f414071bd4d4ed4043e6ff2a4b0c700970b0de1
A.E. Szalo <qCring@gmail.com>
2020-01-18 13:38:13 +0100

    Resources: redundant 3D models removed + consistent scaling

--- src/QuickVtk-Core/SampleData/quickSampleDataMeshes.cpp
+++ src/QuickVtk-Core/SampleData/quickSampleDataMeshes.cpp
@@ -30,44 +30,24 @@ namespace quick {
             return resourceDir + "examples/data/meshes/" + file;
         }
 
-        auto Meshes::getSuzanneOBJ() -> QString {
-            return this->makeDataPath("suzanne.obj");
-        }
-
         auto Meshes::getSuzanneSTL() -> QString {
             return this->makeDataPath("suzanne.stl");
         }
-
-        auto Meshes::getTeapotOFF() -> QString {
-            return this->makeDataPath("teapot.off");
+    
+        auto Meshes::getDragonSTL() -> QString {
+            return this->makeDataPath("dragon.stl");
         }
 
         auto Meshes::getBunnyPLY() -> QString {
             return this->makeDataPath("bunny.ply");
         }
 
-        auto Meshes::getBunnyOBJ() -> QString {
-            return this->makeDataPath("bunny.obj");
-        }
-
-        auto Meshes::getBunnySTL() -> QString {
-            return this->makeDataPath("bunny.stl");
-        }
-
         auto Meshes::getAOBJ() -> QString {
             return this->makeDataPath("a.obj");
         }
 
-        auto Meshes::getASTL() -> QString {
-            return this->makeDataPath("a.stl");
-        }
-
         auto Meshes::getBOBJ() -> QString {
             return this->makeDataPath("b.obj");
-        }
-
-        auto Meshes::getBSTL() -> QString {
-            return this->makeDataPath("b.stl");
         }
     }
 }
--- src/QuickVtk-Core/SampleData/quickSampleDataMeshes.hpp
+++ src/QuickVtk-Core/SampleData/quickSampleDataMeshes.hpp
@@ -9,16 +9,11 @@ namespace quick {
 
         class Meshes : public QObject {
             Q_OBJECT
-            Q_PROPERTY(QString suzanneOBJ READ getSuzanneOBJ CONSTANT);
             Q_PROPERTY(QString suzanneSTL READ getSuzanneSTL CONSTANT);
-            Q_PROPERTY(QString teapotOFF READ getTeapotOFF CONSTANT);
             Q_PROPERTY(QString bunnyPLY READ getBunnyPLY CONSTANT);
-            Q_PROPERTY(QString bunnyOBJ READ getBunnyOBJ CONSTANT);
-            Q_PROPERTY(QString bunnySTL READ getBunnySTL CONSTANT);
+            Q_PROPERTY(QString dragonSTL READ getDragonSTL CONSTANT);
             Q_PROPERTY(QString aOBJ READ getAOBJ CONSTANT);
-            Q_PROPERTY(QString aSTL READ getASTL CONSTANT);
             Q_PROPERTY(QString bOBJ READ getBOBJ CONSTANT);
-            Q_PROPERTY(QString bSTL READ getBSTL CONSTANT);
         private:
             auto makeDataPath(const QString&) -> QString;
             static Meshes* instance;
@@ -26,16 +21,11 @@ namespace quick {
             static Qml::Register::Class<Meshes> Register;
             static auto Create() -> void;
             static auto GetInstance() -> Meshes*;
-            auto getSuzanneOBJ() -> QString;
             auto getSuzanneSTL() -> QString;
-            auto getTeapotOFF() -> QString;
             auto getBunnyPLY() -> QString;
-            auto getBunnyOBJ() -> QString;
-            auto getBunnySTL() -> QString;
+            auto getDragonSTL() -> QString;
             auto getAOBJ() -> QString;
-            auto getASTL() -> QString;
             auto getBOBJ() -> QString;
-            auto getBSTL() -> QString;
         };
     }
 }
